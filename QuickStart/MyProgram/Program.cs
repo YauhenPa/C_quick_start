467b677f20476eab5b5123be3b269ab23080de5b
Yauhen <snegok13@gmail.com>
2018-08-14 14:44:59 +0300

    Add Lesson_2

--- QuickStart/MyProgram/Program.cs
+++ QuickStart/MyProgram/Program.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyProgram
+namespace Lesson_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Здаравствуйте. Здесь вы можете выбрать фильмы для просмотра.");
+            Console.WriteLine("Какой жанр вы хотите увидеть? Ужасы или комедии?");
+            string genre = Console.ReadLine();
+            if (genre == "Ужасы")
+            {
+                Console.WriteLine("Хижина в лесу.");
+                Console.WriteLine("Оно.");
+            }
+            else
+            {
+                Console.WriteLine("Один дома.");
+                Console.WriteLine("Тупой и еще тупее.");
+            };
             Console.ReadLine();
         }
     }
