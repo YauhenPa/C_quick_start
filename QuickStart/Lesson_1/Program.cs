099f4799b1b05f5c098f2d8058da1b8a92658173
Yauhen <snegok13@gmail.com>
2018-08-14 14:05:23 +0300

    Add QuickStart

---
+++ QuickStart/Lesson_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
