9964fa95fe3cd0585a4576474b76af27d1709898
Yauhen <snegok13@gmail.com>
2018-08-17 00:41:48 +0300

    Add collision

--- QuickStart/Turtle_Game/Program.cs
+++ QuickStart/Turtle_Game/Program.cs
@@ -31,27 +31,42 @@ namespace Turtle_Game
         static void Main(string[] args)
         {
             GraphicsWindow.BrushColor = "Red";
-            var eat = Shapes.AddRectangle(20, 20);
+            var Rx = 30;
+            var Ry = 30;
+            var eat = Shapes.AddEllipse(Rx, Ry);
           
-            Turtle.X = 300;
-            Turtle.Y = 300;
-
-           
-
+            Turtle.X = 100;
+            Turtle.Y = 100;
+            int eatX = 200;
+            int eatY = 200;
             
             Turtle.PenUp();
             GraphicsWindow.KeyDown += Control;
 
             while (true)
             {
-                int eatX = Turtle.X;
-                int eatY = Turtle.Y;
-                Turtle.Move(10);
-                Shapes.Move(eat, eatX, eatY);
+                Turtle.Move(5);
+                int deltaX = eatX - Turtle.X + Rx/2;
+                int deltaY = eatY - Turtle.Y + Ry/2;
+                if (-Rx/2 < deltaX && deltaX < Rx/2 && Ry/4 < deltaY && deltaY < Ry/2)
+                {
+                    eatY = eatY + 5;   
+                }
+                if (Rx/4 < deltaX && deltaX < Rx/2 && -Ry/2 < deltaY && deltaY < Ry/2)
+                {
+                    eatX = eatX + 5;   
+                }
+                if (-Rx/2 < deltaX && deltaX < Rx/2 && -Ry/2 < deltaY && deltaY < -Ry/4)
+                {
+                    eatY = eatY - 5;    
+                }
+                if (-Rx/2< deltaX && deltaX < -Rx/4 && -Ry/2< deltaY && deltaY < Ry/2)
+                {
+                    eatX = eatX - 5;  
+                }
                 Control();
+                Shapes.Move(eat, eatX, eatY);
             }
-
-      
         }
     }
 }
